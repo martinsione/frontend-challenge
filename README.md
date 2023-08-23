f0c4ec3117767b8a5342efde2312511b9db17723
Martin Sione <martinsione03@gmail.com>
2023-08-23 11:56:33 -0300

    .

---
+++ README.md
@@ -0,0 +1,15 @@
+# BuildWithin Frontend Developer Challenge
+
+API for the challenge: https://randomuser.me
+
+## Requirements
+
+- [ ] Fetch 100 rows of data using the API.
+- [ ] Display the data in a table format. (picture, email, firstName, lastName)
+- [ ] Alternate the background color of the rows.
+- [ ] Sort by clicking on the column header.
+- [ ] Add a search bar to filter the data by first name, last name, or email.
+- [ ] Add the ability to delete a row.
+- [ ] Add the ability to add a row.
+- [ ] Add the ability to edit a row.
+- [ ] Add pagination to the table.
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm flex">
+        <p className="border-neutral-800 static rounded-xl border bg-zinc-800/30 p-4">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">pages/index.tsx</code>
+        </p>
+      </div>
+
+      <div className="text-3xl sm:text-5xl font-medium tracking-tight grow flex items-center">
+        BuildWithin Challenge
+      </div>
+    </main>
+  );
+}
